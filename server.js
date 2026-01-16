dcff7b7226827b5aa52c0dd770732d9440280a37
Ankur Baijal <155554484+ankurbaijal123@users.noreply.github.com>
2026-01-17 00:36:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const apiRouter = require('./routes/api')
 const app = express();
-app.use(cors());
+app.use(cors({
+   origin: [
+      'http://localhost:8080',          // local frontend
+      'https://your-frontend-domain.app' // deployed frontend
+    ],
+    credentials: true,
+});
 app.use(express.json());
 app.use("/", apiRouter)
 
